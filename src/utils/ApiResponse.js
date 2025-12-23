c5a93a3d7971d9976756d025094a8b2ac59be2e6
raut-anuj <personeluse2000@gmail.com>
2025-12-23 21:11:23 +0530

    Delete src directory

--- src/utils/ApiResponse.js
+++
@@ -1,9 +0,0 @@
-export class ApiResponse {
-  constructor(statusCode, data, message = "Success") {
-    this.statusCode = statusCode;
-    this.data = data;
-    this.message = message;
-  }s
-}
-
-// - ApiResponse → "response" (used to send structured success responses)
